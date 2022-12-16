8413bffb9056cd9e7398915c23120425bc64d3c0
SangyunOck <demibun123@naver.com>
2022-12-16 20:55:25 +0900

    chore: response rename

--- packages/cosmos/src/tx.rs
+++ packages/cosmos/src/tx.rs
@@ -1,6 +1,6 @@
-use tendermint_rpc::endpoint::broadcast::tx_async::Response as AsyncResponse;
+use tendermint_rpc::endpoint::broadcast::tx_async::Response;
 use tendermint_rpc::{Client, Error, HttpClient};
 
-pub async fn broadcast(client: &HttpClient, signed_tx: Vec<u8>) -> Result<AsyncResponse, Error> {
+pub async fn broadcast(client: &HttpClient, signed_tx: Vec<u8>) -> Result<Response, Error> {
     client.broadcast_tx_async(signed_tx).await
 }
